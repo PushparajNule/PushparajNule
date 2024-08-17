88f155a210561e60716fa2c00330e996e593a855
PushparajNule <141165361+PushparajNule@users.noreply.github.com>
2024-08-17 20:11:20 +0530

    Create Closure Example.js

---
+++ Skills/JavaScript/Closure Example.js
@@ -0,0 +1,11 @@
+/*Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).*/
+var n = 10;
+
+function counter(){
+    var i = n++;
+    console.log(i);
+};
+
+counter();
+counter();
+counter();
